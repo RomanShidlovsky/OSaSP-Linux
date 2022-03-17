cd69411baffb6a16473cf093ba0dec0897bc24de
RomanShidlovsky <91383472+RomanShidlovsky@users.noreply.github.com>
2022-03-17 18:02:24 +0300

    Create task_04.c

--- lab2/task_04.c
+++ lab2/task_04.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+
 
 int main(int argc, char *argv[]){
 
@@ -13,12 +16,21 @@ int main(int argc, char *argv[]){
     char *n_arg = argv[2];
 
     char* endptr;
-    int n =strtol(n_arg, &endptr, 10);
-    if (endptr[0]!='\0')
-    {
-        perror("Last paramater should have int value\n");
-        return -2;
+
+    errno = 0;
+
+    long n = strtol(n_arg, &endptr, 10);
+
+    if ((errno == ERANGE && (n == LONG_MAX || n == LONG_MIN)) || (errno != 0 && n == 0)) {
+       perror("strtol");
+       return -2;
     }
+
+    if (endptr == n_arg) {
+       fprintf(stderr, "Last parameter should have int value\n");
+       return -2;
+    }
+
 
     FILE* file = fopen(file_name,"r");
 
@@ -28,7 +40,7 @@ int main(int argc, char *argv[]){
         return -3;
     }
 
-    int lines_printed = 0;
+    long lines_printed = 0;
     char symbol;
     while((symbol = fgetc(file))!=EOF){
         if (symbol=='\n')
@@ -51,4 +63,3 @@ int main(int argc, char *argv[]){
     return 0;
 
 }
-
